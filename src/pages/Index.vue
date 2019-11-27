f6150ef266d529694f7f71e184120e349f3d89a7
Baptiste Boucher <baptiste.boucher@gmail.com>
2019-11-27 08:21:18 +0100

    add eslint

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,145 +1,311 @@
 <template>
   <div class="bg-blue-100 text-xl overflow-hidden">
     <div class="flex pt-16 justify-center">
-          <svg style="animation: circle 40s linear infinite;" viewBox="0 0 100 110" width="200" height="200">
-  <defs>
-    <path id="circle"
-      d="
+      <svg
+        style="animation: circle 40s linear infinite;"
+        viewBox="0 0 100 110"
+        width="200"
+        height="200"
+      >
+        <defs>
+          <path
+            id="circle"
+            d="
         M 50, 50
         m -37, 0
         a 37,37 0 1,1 74,0
-        a 37,37 0 1,1 -74,0"/>
-  </defs>
-  <text style="font-family: Roboto;
+        a 37,37 0 1,1 -74,0"
+          />
+        </defs>
+        <text
+          style="font-family: Roboto;
                font-size  : 19;
                font-weight: bold;
-               fill       : #F6E05E;">
-    <textPath xlink:href="#circle">
-       - FRIENDSHIP - WEEKEND
-    </textPath>
-  </text>
-</svg>
+               fill       : #F6E05E;"
+        >
+          <textPath xlink:href="#circle">
+            - FRIENDSHIP - WEEKEND
+          </textPath>
+        </text>
+      </svg>
     </div>
 
     <section class="p-16 pb-64">
       <div class="text-center w-2/4 m-auto text-blue-800">
         <p>Si vous lisez ce message, c’est qu’on vous aime très fort.</p>
         <p>Vous êtes comme notre famille.</p>
-        <br>
-        <p>Mais pourquoi attendre un mariage pour rassembler les gens qu’on aime ?</p>
-        <p>Puisqu’il faut une bonne raison, nos 2 ans de PACS feront l’affaire !</p>
-        <br>
-        <p>Venez prendre une bouffée d’amitié, le temps d’un week-end avec nous.</p>
+        <br />
+        <p>
+          Mais pourquoi attendre un mariage pour rassembler les gens qu’on aime
+          ?
+        </p>
+        <p>
+          Puisqu’il faut une bonne raison, nos 2 ans de PACS feront l’affaire !
+        </p>
+        <br />
+        <p>
+          Venez prendre une bouffée d’amitié, le temps d’un week-end avec nous.
+        </p>
       </div>
     </section>
 
     <section class="relative bg-white p-16 text-center text-gray-600">
-<svg class="absolute" style="left: 0; top: -210px" width="1562" height="210" viewBox="0 0 1562 210" fill="none" xmlns="http://www.w3.org/2000/svg">
-<mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="1562" height="210">
-<rect width="1562" height="210" fill="black"/>
-</mask>
-<g mask="url(#mask0)">
-<circle cx="136.5" cy="151.5" r="143.5" fill="white"/>
-<circle cx="280" cy="180" r="80" fill="white"/>
-<circle cx="388" cy="260" r="80" fill="white"/>
-<circle cx="514" cy="215" r="80" fill="white"/>
-<circle cx="630" cy="260" r="80" fill="white"/>
-<circle cx="774.5" cy="231.5" r="131.5" fill="white"/>
-<circle cx="1024" cy="260" r="80" fill="white"/>
-<circle cx="1150" cy="215" r="80" fill="white"/>
-<circle cx="1266" cy="260" r="80" fill="white"/>
-<circle cx="923.5" cy="283.5" r="131.5" fill="white"/>
-<circle cx="1423.5" cy="202.5" r="131.5" fill="white"/>
-<path d="M-7 394.425V163L41.8597 204.119H1555V425H-7V394.425Z" fill="white"/>
-</g>
-</svg>
-<h2 class="text-center font-title text-gray-700">Le lieu</h2>
-<p>A 1 heure de Bordeaux, et 5 heures de Paris en train se cache un vrai paradis.</p>
-<p>Nous avons hâte de vous y accueillir !</p>
-<br>
-<p>Quelques  photos prisent durant nos derniers séjours: </p>
-    <div class="gallery pt-8">
-      <img src="../assets/image2.jpg" 
-      class="tile" id="image2" />
-      <img src="../assets/image3.jpg" class="tile" id="image3" />
-      <img src="../assets/image4.jpg" class="tile" id="image4" />
-      <img src="../assets/image5.jpg" class="tile" id="image5" />
-      <img src="../assets/image6.jpg" class="tile" id="image6" />
-      <img src="../assets/image7.jpg" class="tile" id="image7" />
-    </div>
+      <svg
+        class="absolute"
+        style="left: 0; top: -210px"
+        width="1562"
+        height="210"
+        viewBox="0 0 1562 210"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <mask
+          id="mask0"
+          mask-type="alpha"
+          maskUnits="userSpaceOnUse"
+          x="0"
+          y="0"
+          width="1562"
+          height="210"
+        >
+          <rect width="1562" height="210" fill="black" />
+        </mask>
+        <g mask="url(#mask0)">
+          <circle cx="136.5" cy="151.5" r="143.5" fill="white" />
+          <circle cx="280" cy="180" r="80" fill="white" />
+          <circle cx="388" cy="260" r="80" fill="white" />
+          <circle cx="514" cy="215" r="80" fill="white" />
+          <circle cx="630" cy="260" r="80" fill="white" />
+          <circle cx="774.5" cy="231.5" r="131.5" fill="white" />
+          <circle cx="1024" cy="260" r="80" fill="white" />
+          <circle cx="1150" cy="215" r="80" fill="white" />
+          <circle cx="1266" cy="260" r="80" fill="white" />
+          <circle cx="923.5" cy="283.5" r="131.5" fill="white" />
+          <circle cx="1423.5" cy="202.5" r="131.5" fill="white" />
+          <path
+            d="M-7 394.425V163L41.8597 204.119H1555V425H-7V394.425Z"
+            fill="white"
+          />
+        </g>
+      </svg>
+      <h2 class="text-center font-title text-gray-700">
+        Le lieu
+      </h2>
+      <p>
+        A 1 heure de Bordeaux, et 5 heures de Paris en train se cache un vrai
+        paradis.
+      </p>
+      <p>Nous avons hâte de vous y accueillir !</p>
+      <br />
+      <p>Quelques photos prisent durant nos derniers séjours:</p>
+      <div class="gallery pt-8">
+        <img src="../assets/image2.jpg" id="image2" class="tile" />
+        <img src="../assets/image3.jpg" id="image3" class="tile" />
+        <img src="../assets/image4.jpg" id="image4" class="tile" />
+        <img src="../assets/image5.jpg" id="image5" class="tile" />
+        <img src="../assets/image6.jpg" id="image6" class="tile" />
+        <img src="../assets/image7.jpg" id="image7" class="tile" />
+      </div>
 
-    <svg class="absolute" style="left: 0; bottom: -210px" width="1562" height="210" viewBox="0 0 1562 210" fill="none" xmlns="http://www.w3.org/2000/svg">
-<mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="1562" height="210">
-<rect x="1562" y="210" width="1562" height="210" transform="rotate(180 1562 210)" fill="black"/>
-</mask>
-<g mask="url(#mask0)">
-<circle cx="1425.5" cy="58.5" r="143.5" transform="rotate(180 1425.5 58.5)" fill="white"/>
-<circle cx="1282" cy="29.9999" r="80" transform="rotate(180 1282 29.9999)" fill="white"/>
-<circle cx="1174" cy="-50" r="80" transform="rotate(180 1174 -50)" fill="white"/>
-<circle cx="1048" cy="-4.99999" r="80" transform="rotate(180 1048 -4.99999)" fill="white"/>
-<circle cx="932" cy="-50" r="80" transform="rotate(180 932 -50)" fill="white"/>
-<circle cx="787.5" cy="-21.5" r="131.5" transform="rotate(180 787.5 -21.5)" fill="white"/>
-<circle cx="538" cy="-50" r="80" transform="rotate(180 538 -50)" fill="white"/>
-<circle cx="412" cy="-4.99993" r="80" transform="rotate(180 412 -4.99993)" fill="white"/>
-<circle cx="296" cy="-50" r="80" transform="rotate(180 296 -50)" fill="white"/>
-<circle cx="638.5" cy="-73.5" r="131.5" transform="rotate(180 638.5 -73.5)" fill="white"/>
-<circle cx="138.5" cy="7.50007" r="131.5" transform="rotate(180 138.5 7.50007)" fill="white"/>
-<path d="M1569 -184.425L1569 46.9999L1520.14 5.88123L7 5.88136L6.99998 -215L1569 -215L1569 -184.425Z" fill="white"/>
-</g>
-</svg>
+      <svg
+        class="absolute"
+        style="left: 0; bottom: -210px"
+        width="1562"
+        height="210"
+        viewBox="0 0 1562 210"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <mask
+          id="mask0"
+          mask-type="alpha"
+          maskUnits="userSpaceOnUse"
+          x="0"
+          y="0"
+          width="1562"
+          height="210"
+        >
+          <rect
+            x="1562"
+            y="210"
+            width="1562"
+            height="210"
+            transform="rotate(180 1562 210)"
+            fill="black"
+          />
+        </mask>
+        <g mask="url(#mask0)">
+          <circle
+            cx="1425.5"
+            cy="58.5"
+            r="143.5"
+            transform="rotate(180 1425.5 58.5)"
+            fill="white"
+          />
+          <circle
+            cx="1282"
+            cy="29.9999"
+            r="80"
+            transform="rotate(180 1282 29.9999)"
+            fill="white"
+          />
+          <circle
+            cx="1174"
+            cy="-50"
+            r="80"
+            transform="rotate(180 1174 -50)"
+            fill="white"
+          />
+          <circle
+            cx="1048"
+            cy="-4.99999"
+            r="80"
+            transform="rotate(180 1048 -4.99999)"
+            fill="white"
+          />
+          <circle
+            cx="932"
+            cy="-50"
+            r="80"
+            transform="rotate(180 932 -50)"
+            fill="white"
+          />
+          <circle
+            cx="787.5"
+            cy="-21.5"
+            r="131.5"
+            transform="rotate(180 787.5 -21.5)"
+            fill="white"
+          />
+          <circle
+            cx="538"
+            cy="-50"
+            r="80"
+            transform="rotate(180 538 -50)"
+            fill="white"
+          />
+          <circle
+            cx="412"
+            cy="-4.99993"
+            r="80"
+            transform="rotate(180 412 -4.99993)"
+            fill="white"
+          />
+          <circle
+            cx="296"
+            cy="-50"
+            r="80"
+            transform="rotate(180 296 -50)"
+            fill="white"
+          />
+          <circle
+            cx="638.5"
+            cy="-73.5"
+            r="131.5"
+            transform="rotate(180 638.5 -73.5)"
+            fill="white"
+          />
+          <circle
+            cx="138.5"
+            cy="7.50007"
+            r="131.5"
+            transform="rotate(180 138.5 7.50007)"
+            fill="white"
+          />
+          <path
+            d="M1569 -184.425L1569 46.9999L1520.14 5.88123L7 5.88136L6.99998 -215L1569 -215L1569 -184.425Z"
+            fill="white"
+          />
+        </g>
+      </svg>
     </section>
     <section class="pt-32 py-16 text-center text-blue-600">
-  <h2 class="text-blue-700 pt-16">Au programme</h2>
-  <div class="flex text-left pb-32 justify-center">
-      <div>
-      <p>Plage</p>
-  <p>Piscine</p>
-  <p>Olympiades</p>
-  <p>Marchés</p>
-  <p>Aperos</p>
-  <p>Bouee tractee</p>
-  </div>
-<div class="ml-32">
-    <p>Jeux de societe</p>
-  <p>Surf</p>
-  <p>Yoga</p>
-  <p>Tennis</p>
-  <p>Balade a velo</p>
-  <p>Ping-Pong</p>
-</div>
-  </div>
-
-
-
-
+      <h2 class="text-blue-700 pt-16">
+        Au programme
+      </h2>
+      <div class="flex text-left pb-32 justify-center">
+        <div>
+          <p>Plage</p>
+          <p>Piscine</p>
+          <p>Olympiades</p>
+          <p>Marchés</p>
+          <p>Aperos</p>
+          <p>Bouee tractee</p>
+        </div>
+        <div class="ml-32">
+          <p>Jeux de societe</p>
+          <p>Surf</p>
+          <p>Yoga</p>
+          <p>Tennis</p>
+          <p>Balade a velo</p>
+          <p>Ping-Pong</p>
+        </div>
+      </div>
     </section>
-  <section class="relative bg-yellow-400 p-16 pb-64 text-center text-yellow-700">
-    <svg class="absolute" style="left: 0; top: -137px" width="1580" height="137" viewBox="0 0 1580 137" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M353.5 60.5001C189.5 -47.4999 49.8333 15.5001 0.5 60.5001V137H1580V89.5C1239.5 124.833 517.5 168.5 353.5 60.5001Z" fill="#F6E05E"/>
-</svg>
-    <h2 class="text-yellow-800">On fait ça quand ?</h2>
+    <section
+      class="relative bg-yellow-400 p-16 pb-64 text-center text-yellow-700"
+    >
+      <svg
+        class="absolute"
+        style="left: 0; top: -137px"
+        width="1580"
+        height="137"
+        viewBox="0 0 1580 137"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M353.5 60.5001C189.5 -47.4999 49.8333 15.5001 0.5 60.5001V137H1580V89.5C1239.5 124.833 517.5 168.5 353.5 60.5001Z"
+          fill="#F6E05E"
+        />
+      </svg>
+      <h2 class="text-yellow-800">
+        On fait ça quand ?
+      </h2>
 
-    <p>Week-end prolongé de l’Ascension, 
-du jeudi 21 au dimanche 24 mai 2020.</p>
-<p>Ajouter à mon calendrier</p>
-  </section>
+      <p>
+        Week-end prolongé de l’Ascension, du jeudi 21 au dimanche 24 mai 2020.
+      </p>
+      <p>Ajouter à mon calendrier</p>
+    </section>
 
-  <section style="background-color: #A0E1F2" class="relative px-16 pb-64 text-center text-blue-800">
-    <svg class="absolute" style="left: 0; top: -172px" width="1581" height="172" viewBox="0 0 1581 172" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1429.98 7.10428C1488.37 7.10428 1554.99 23.5232 1581 31.7327V83H0V64.9057V7.10428C9.87524 12.6331 44.2218 25.2992 102.606 31.7327C175.586 39.7744 218.219 7.10428 303.483 7.10428C388.747 7.10428 425.598 49.827 531.095 17.6594C636.591 -14.5083 664.049 7.10428 720.41 7.10428C776.771 7.10428 815.79 43.7956 864.926 31.7327C982.706 -0.435014 1008 -2.44549 1082.42 7.10428C1156.85 16.6541 1208.87 38.7693 1291.97 31.7327C1375.07 24.696 1357 7.10428 1429.98 7.10428Z" fill="#3EB7D9"/>
-<path d="M151.019 46.1043C92.6345 46.1043 26.0128 62.5232 0 70.7327V122H1581V103.906V46.1043C1571.12 51.6331 1536.78 64.2992 1478.39 70.7327C1405.41 78.7744 1362.78 46.1043 1277.52 46.1043C1192.25 46.1043 1155.4 88.827 1049.91 56.6594C944.409 24.4917 916.951 46.1043 860.59 46.1043C804.229 46.1043 765.21 82.7956 716.074 70.7327C598.294 38.565 573.004 36.5545 498.579 46.1043C424.153 55.6541 372.128 77.7693 289.031 70.7327C205.935 63.696 223.999 46.1043 151.019 46.1043Z" fill="#5ECDEC"/>
-<path d="M1429.98 96.1043C1488.37 96.1043 1554.99 112.523 1581 120.733V172H0V153.906V96.1043C9.87524 101.633 44.2218 114.299 102.606 120.733C175.586 128.774 218.219 96.1043 303.483 96.1043C388.747 96.1043 425.598 138.827 531.095 106.659C636.591 74.4917 664.049 96.1043 720.41 96.1043C776.771 96.1043 815.79 132.796 864.926 120.733C982.706 88.565 1008 86.5545 1082.42 96.1043C1156.85 105.654 1208.87 127.769 1291.97 120.733C1375.07 113.696 1357 96.1043 1429.98 96.1043Z" fill="#A0E1F2"/>
-</svg>
-    <h2>Next steps</h2>
-    <p>Il ne vous reste plus qu’à nous confirmer votre venue.</p>
-    <br>
-    <p>Si possible avant le 15 décembre 2019.</p>
-    <br>
-    <p>Pour les plus hésitants, dites-vous que c’est tout comme un mariage.</p>
-    <p>Vous ne voudriez pas rater ça ! </p>
-  </section>
+    <section
+      style="background-color: #A0E1F2"
+      class="relative px-16 pb-64 text-center text-blue-800"
+    >
+      <svg
+        class="absolute"
+        style="left: 0; top: -172px"
+        width="1581"
+        height="172"
+        viewBox="0 0 1581 172"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1429.98 7.10428C1488.37 7.10428 1554.99 23.5232 1581 31.7327V83H0V64.9057V7.10428C9.87524 12.6331 44.2218 25.2992 102.606 31.7327C175.586 39.7744 218.219 7.10428 303.483 7.10428C388.747 7.10428 425.598 49.827 531.095 17.6594C636.591 -14.5083 664.049 7.10428 720.41 7.10428C776.771 7.10428 815.79 43.7956 864.926 31.7327C982.706 -0.435014 1008 -2.44549 1082.42 7.10428C1156.85 16.6541 1208.87 38.7693 1291.97 31.7327C1375.07 24.696 1357 7.10428 1429.98 7.10428Z"
+          fill="#3EB7D9"
+        />
+        <path
+          d="M151.019 46.1043C92.6345 46.1043 26.0128 62.5232 0 70.7327V122H1581V103.906V46.1043C1571.12 51.6331 1536.78 64.2992 1478.39 70.7327C1405.41 78.7744 1362.78 46.1043 1277.52 46.1043C1192.25 46.1043 1155.4 88.827 1049.91 56.6594C944.409 24.4917 916.951 46.1043 860.59 46.1043C804.229 46.1043 765.21 82.7956 716.074 70.7327C598.294 38.565 573.004 36.5545 498.579 46.1043C424.153 55.6541 372.128 77.7693 289.031 70.7327C205.935 63.696 223.999 46.1043 151.019 46.1043Z"
+          fill="#5ECDEC"
+        />
+        <path
+          d="M1429.98 96.1043C1488.37 96.1043 1554.99 112.523 1581 120.733V172H0V153.906V96.1043C9.87524 101.633 44.2218 114.299 102.606 120.733C175.586 128.774 218.219 96.1043 303.483 96.1043C388.747 96.1043 425.598 138.827 531.095 106.659C636.591 74.4917 664.049 96.1043 720.41 96.1043C776.771 96.1043 815.79 132.796 864.926 120.733C982.706 88.565 1008 86.5545 1082.42 96.1043C1156.85 105.654 1208.87 127.769 1291.97 120.733C1375.07 113.696 1357 96.1043 1429.98 96.1043Z"
+          fill="#A0E1F2"
+        />
+      </svg>
+      <h2>Next steps</h2>
+      <p>Il ne vous reste plus qu’à nous confirmer votre venue.</p>
+      <br />
+      <p>Si possible avant le 15 décembre 2019.</p>
+      <br />
+      <p>
+        Pour les plus hésitants, dites-vous que c’est tout comme un mariage.
+      </p>
+      <p>Vous ne voudriez pas rater ça !</p>
+    </section>
   </div>
-
 </template>
 
 <script>
@@ -147,12 +313,12 @@ export default {};
 </script>
 
 <style>
-@keyframes circle{
-  from{
-    transform:rotate(0deg)
+@keyframes circle {
+  from {
+    transform: rotate(0deg);
   }
-  to{
-    transform:rotate(360deg)
+  to {
+    transform: rotate(360deg);
   }
 }
 
